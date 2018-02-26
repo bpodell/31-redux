e16e5df30694ffb5779916ca07872879228cbf50
billodell <billodell5@gmail.com>
2018-02-26 15:11:30 -0800

    delete working

---
+++ lab-bill/src/components/category/category-item.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import CategoryForm from './category-form';
+// import { renderIf } from '../lib/index';
+
+class CategoryItem extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      display:false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChangeState = this.handleChangeState.bind(this);
+    this.getOrSetState = this.getOrSetState.bind(this);
+  }
+
+  handleClick(event){
+    event.preventDefault();
+    this.props.destroy(this.props.category);
+  }
+
+  handleChangeState(){
+    this.setState({display:true});
+  }
+
+  getOrSetState() {
+    return {
+      state: this.state,
+      setState: this.setState.bind(this),
+    };
+  }
+
+  render(){
+    console.log('item level',this.props);
+    return(
+        <li>
+          <h2>{this.props.category.name}</h2>
+          <p>{this.props.category.budget}</p>
+          <button type='submit' onClick={this.handleClick}> delete </button>
+          <CategoryForm buttonText='update' update={this.props.update}/>
+        </li>
+    );
+  }
+}
+export default CategoryItem;
